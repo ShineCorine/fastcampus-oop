6b8a02a523ede14f3390173f191ce34b8968fb33
ShineCorine <imsjm81@gmail.com>
2023-11-21 09:47:11 +0900

    udpate #4 SortService
    구현체 출력 추가

--- fastcampus-oop/src/test/java/com/fastcampus/javaoop/service/SortServiceTest.java
+++ fastcampus-oop/src/test/java/com/fastcampus/javaoop/service/SortServiceTest.java
@@ -1,4 +1,23 @@
+package com.fastcampus.javaoop.service;
+
+import com.fastcampus.javaoop.logic.JavaSort;
+import com.fastcampus.javaoop.logic.SelectionSort;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SortServiceTest {
-  
+
+    private SortService sut = new SortService(new SelectionSort<String>());
+
+    @Test
+    void test(){
+
+        List<String> actual = sut.doSort(List.of("3", "5", "1"));
+
+        assertEquals(List.of("1", "3", "5"), actual);
+    }
+
 }
